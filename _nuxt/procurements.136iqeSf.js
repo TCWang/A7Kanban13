db34ef03edc3dc7867d26863ecf7a05db1172542
TC Wang <wangtct@hotmail.com>
2023-12-27 15:57:17 +0800

    Updates

---
+++ _nuxt/procurements.136iqeSf.js
@@ -0,0 +1,150 @@
+const t=[{id:"N001",category:"住屋居家",item:"建商附贈設備使用說明書",description:`                              <div class="flex flex-col md:flex-row">\r
+\r
+<div class="w-full md:w-1/2 mt-2">\r
+\r
+                                    <h3>室內對講機</h3>\r
+                                    <ul>\r
+                                        <li>型號：<a href="http://www.hometek.tw/">Hometek 歐益科技</a> HA-8117E 彩色影像室內對講機</li>\r
+                                        <li>使用手冊：<a href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-105.pdf">HA-8117E</a>\r
+                                            使用說明書, <a href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-106.pdf">HA-8117</a>\r
+                                            使用說明書</li>\r
+                                        <li>教學影片： HA-8117 <a\r
+                                                href="https://www.youtube.com/watch?v=MU2YtL4dglQ">功能鍵介紹</a>，HA-8117 <a\r
+                                                href="https://www.youtube.com/watch?v=M-GzJY1g9co">操作說明</a>，<a\r
+                                                href="https://www.youtube.com/watch?v=lqKEofIoJoE">HA-8117 保全設定與解除</a>\r
+                                        </li>\r
+                                    </ul>\r
+                                    <h3>廚具設備</h3>\r
+                                    <ul>\r
+                                        <li><a href="http://www.rinnai.com.tw/">Linna 林內</a>排油煙機 RH-8079 <a\r
+                                                href="https://sofree.cc/range-hood-cleaner/">油網清洗</a> </li>\r
+                                        <li>Linna 林內檯面型<a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-102.pdf">瓦斯爐</a>&nbsp;MB-220GH-TY</li>\r
+                                        <li>Linna 林內電熱式<a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-101.pdf">烘碗機</a>&nbsp;MKD-5055(P)\r
+                                        </li>\r
+                                    </ul>\r
+                                    <h3>浴室暖房乾燥設備</h3>\r
+                                    <ul>\r
+                                        <li><a\r
+                                                href="http://www.daguan-tech.com.tw/GoWeb/include/index.php?Page=3-1-2&amp;product01=18545364945861e0a4d0653">LifeGear\r
+                                                樂奇</a>：<a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-104-A.pdf">浴室暖房/換氣/乾燥設備</a>:\r
+                                            &nbsp; <a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-104-B.pdf">BD-125W1</a>/WL1/R1\r
+                                        </li>\r
+                                    </ul>\r
+</div>\r
+<div class="w-full md:w-1/2 mt-2">\r
+                                   <h3> 飲水系統</h3>\r
+                                    <ul>\r
+                                        <li>3M CUNO 家用生飲水系統 <a\r
+                                                href="https://multimedia.3m.com/mws/media/499085O/water-at-its-finest-fm-dws-1500-spec-sheet.pdf">FM\r
+                                                DWS 1500</a></li>\r
+                                        <li>\r
+                                            安裝教學： <a href="https://www.youtube.com/watch?v=qfx2KHqWUFE">3M 濾水器濾心安裝教學</a>\r
+                                        </li>\r
+                                    </ul>\r
+                                    <h3>洗淨便座</h3>\r
+                                    <ul>\r
+                                        <li><a\r
+                                                href="https://www.panasonic.com/tw/consumer/health/toilet/hotinstant/dl-ph20tws.html">Panasonic\r
+                                            </a>家庭用 溫水<a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-107.pdf">洗淨便座</a>DL-F509BTWS&nbsp;\r
+                                        </li>\r
+                                        <li>Panasonic <a\r
+                                                href="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-file/MAN-108.pdf">DL-EH30</a> 使用手冊\r
+                                        </li>\r
+                                        <li>教學影片：洗淨便座<a href="https://www.youtube.com/watch?v=bo8AKW4qapo">介紹</a>，洗淨便座<a\r
+                                                href="https://www.youtube.com/watch?v=fKDWSX1AUjU">安裝步驟</a></li>\r
+                                    </ul>\r
+</div>\r
+                                </div>`},{id:"N002",category:"住屋居家",item:"寢具尺寸規格",description:`<div class="flex flex-col md:flex-row">\r
+<div class="w-full md:w-1/2 mt-2">\r
+                               <h2>床墊的尺寸</h2> \r
+                                <h3>台灣規格</h3>\r
+                                <table class="min-w-full divide-y divide-zinc-100">\r
+                                    <thead>\r
+                                        <tr>\r
+                                            <th>種類</th>\r
+                                            <th>寬 x 長 （呎）</th>\r
+                                            <th>寬 x 長 （公分）</th>\r
+                                        </tr>\r
+                                    </thead>\r
+                                    <tbody>\r
+                                        <tr>\r
+                                            <td>標準單人</td>\r
+                                            <td>3 x 6.2 呎</td>\r
+                                            <td>90 x 188 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>單人加大</td>\r
+                                            <td>3.5 x 6.2 呎</td>\r
+                                            <td>107 x 188 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>標準雙人</td>\r
+                                            <td>5 x 6.2 呎</td>\r
+                                            <td>152 x 188 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>雙人加大 Queen</td>\r
+                                            <td>6 x 6.2 呎 </td>\r
+                                            <td>180 x 188 公分 *</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>雙人特大 King</td>\r
+                                            <td>6 x 7 呎</td>\r
+                                            <td>180 x 210 公分</td>\r
+                                        </tr>\r
+                                    </tbody>\r
+                                </table> <br>\r
+                               <h3> 國外尺寸</h3>\r
+                                <table class="min-w-full divide-y divide-zinc-100">\r
+                                    <thead>\r
+                                        <tr>\r
+                                            <th>種類</th>\r
+                                            <th>歐洲 IKEA</th>\r
+                                            <th>日本 MUJI</th>\r
+                                            <th>美國 席夢思</th>\r
+                                        </tr>\r
+                                    </thead>\r
+                                    <tbody>\r
+                                        <tr>\r
+                                            <td>標準單人 Twin</td>\r
+                                            <td>80 x 200, 90 x 190, 90 x 200 公分</td>\r
+                                            <td>97 x 195 公分</td>\r
+                                            <td>96 x 189 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>單人加大 Twin XL</td>\r
+                                            <td>120 x 200 公分</td>\r
+                                            <td>120 x 195 公分</td>\r
+                                            <td>96 x 201 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>標準雙人 Full </td>\r
+                                            <td> 140 x 200, 150 x 190, 150 x 200公分</td>\r
+                                            <td>140 x 195 公分 *</td>\r
+                                            <td>134 x 189 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>雙人加大 Queen</td>\r
+                                            <td>180 x 200 公分 </td>\r
+                                            <td>160 x 195 公分</td>\r
+                                            <td>152 x 201 公分</td>\r
+                                        </tr>\r
+                                        <tr>\r
+                                            <td>雙人特大 King</td>\r
+                                            <td>N.A.</td>\r
+                                            <td>N.A.</td>\r
+                                            <td>193 x 201 公分</td>\r
+                                        </tr>\r
+                                    </tbody>\r
+                                </table>\r
+</div>\r
+<div class="w-full md:w-1/2 mt-2">\r
+                            <h2>棉被/床包 尺寸</h2>\r
+                                <img src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/procurement/PRO-001.png" class="img-fluid" style="width:800px">\r
+</div>\r
+</div>`}];export{t as p};
---
+++ _nuxt/useServices.q8u1ZgCS.js
@@ -0,0 +1,1 @@
+import{s}from"./services.WZFQiQCY.js";import{a as r,t as e}from"./typecs.qIdT0Xe8.js";const o=()=>({services:s,areas:r,typecs:e});export{o as u};
