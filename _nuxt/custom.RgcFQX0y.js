a5399e475eccb863b1a0820a0625eefceb4eb2bd
TC Wang <wangtct@hotmail.com>
2024-01-05 09:18:52 +0800

    Updates

--- _nuxt/custom.RgcFQX0y.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./NavBar.NXqkSsxy.js";import{_ as t,c as a,b as c,a as n,a9 as r,o as _}from"./entry.VttMrVb9.js";const l={},m={class:"mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-full"};function p(e,x){const o=s;return _(),a("div",null,[c(o),n("div",m,[r(e.$slots,"default")])])}const u=t(l,[["render",p]]);export{u as default};
