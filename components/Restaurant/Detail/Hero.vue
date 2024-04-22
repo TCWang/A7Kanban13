3dd4a35a76f6662ef62838fbadff904a539da523
TC Wang <wangtct@hotmail.com>
2024-04-23 06:34:09 +0800

    img

--- components/Restaurant/Detail/Hero.vue
+++ components/Restaurant/Detail/Hero.vue
@@ -12,7 +12,7 @@
     /> -->
     <div class="w-full md:w-1/2">
       <h1 class="mt-1">
-        N{{ restaurant.id }} {{ restaurant.type }} - {{ restaurant.name }}
+        {{ restaurant.id }} {{ restaurant.type }} - {{ restaurant.name }}
       </h1>
       <div class="text-slate-500 text-lg mt-3 border-b pb-5 justify-between">
         <div class="flex-col">
